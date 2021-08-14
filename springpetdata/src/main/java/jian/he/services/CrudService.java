80f2209f3126475865a8da1280050fd0d802fb72
jianheMark <jianhe.mark@pm.me>
2021-08-14 14:17:12 +0530

    refactoring to common crud interface. Closes #3

---
+++ springpetdata/src/main/java/jian/he/services/CrudService.java
@@ -0,0 +1,15 @@
+package jian.he.services;
+
+import java.util.Set;
+public interface CrudService<T, ID>{
+
+    T findById(ID id);
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+
+    Set<T> findAll();
+
+}
