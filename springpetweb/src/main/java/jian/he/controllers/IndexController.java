2b118daa32dbfd0f979bdc5260e9f6ff0f70ccfc
jianheMark <jianhe.mark@pm.me>
2021-08-14 17:19:00 +0530

    add Index and controller. closes #4.

---
+++ springpetweb/src/main/java/jian/he/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package jian.he.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"index.html","","/","index"})
+    public String index(){
+        return "index";
+    }
+
+}
