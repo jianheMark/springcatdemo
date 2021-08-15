ea63fcc41575cfff25f7e8e7bec0767cbb1f1845
jianheMark <jianhe.mark@pm.me>
2021-08-15 12:35:40 +0530

    Load Bootstrap on startup. closes #13.

---
+++ springpetweb/src/main/java/jian/he/bootstrap/DataLoader.java
@@ -0,0 +1,54 @@
+package jian.he.bootstrap;
+
+import jian.he.model.Owner;
+import jian.he.model.Vet;
+import jian.he.services.OwnerService;
+import jian.he.services.VetService;
+import jian.he.services.map.OwnerServiceMap;
+import jian.he.services.map.VetServiceMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    @Autowired
+    public DataLoader(OwnerService ownerService,VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Dionne");
+        owner1.setLastName("Warwick");
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner1.setId(2L);
+        owner1.setFirstName("The");
+        owner1.setLastName("Crystals");
+        ownerService.save(owner2);
+        System.out.println("|||| Loading owners.");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet1.setId(2L);
+        vet1.setFirstName("Jessie");
+        vet1.setLastName("Porter");
+        vetService.save(vet2);
+        System.out.println("Loading Vets");
+
+    }
+}
