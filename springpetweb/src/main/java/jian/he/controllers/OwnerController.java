3261348df74adb32e240be3a1d8e190994250e14
jianheMark <jianhe.mark@pm.me>
2021-08-15 21:52:51 +0530

    Create Owner Page and Controller. List All owner in index page. closes #8.

--- springpetweb/src/main/java/jian/he/controllers/OwnerController.java
+++ springpetweb/src/main/java/jian/he/controllers/OwnerController.java
@@ -1,14 +1,22 @@
 package jian.he.controllers;
 
+import jian.he.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @GetMapping({"/owners","/owners/index","/owners/index.html"})
 
-    public String listowner(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
