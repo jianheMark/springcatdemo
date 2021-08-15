bdec90071b8bf1ff4b70794be280ad2bf6f57154
jianheMark <jianhe.mark@pm.me>
2021-08-15 22:52:35 +0530

    List All vets on index page. closes #14.

--- springpetweb/src/main/java/jian/he/bootstrap/DataLoader.java
+++ springpetweb/src/main/java/jian/he/bootstrap/DataLoader.java
@@ -31,9 +31,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("The");
-        owner1.setLastName("Crystals");
+        owner2.setId(2L);
+        owner2.setFirstName("The");
+        owner2.setLastName("Crystals");
         ownerService.save(owner2);
         System.out.println("|||| Loading owners.");
 
@@ -44,9 +44,9 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet1.setId(2L);
-        vet1.setFirstName("Jessie");
-        vet1.setLastName("Porter");
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
         vetService.save(vet2);
         System.out.println("Loading Vets");
 
--- springpetweb/src/main/java/jian/he/controllers/OwnerController.java
+++ springpetweb/src/main/java/jian/he/controllers/OwnerController.java
@@ -1,17 +1,20 @@
 package jian.he.controllers;
 
 import jian.he.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OwnerController {
-    private final OwnerService ownerService;
-
-    public OwnerController(OwnerService ownerService) {
-        this.ownerService = ownerService;
-    }
+//    private final OwnerService ownerService;
+//
+//    public OwnerController(OwnerService ownerService) {
+//        this.ownerService = ownerService;
+//    }
+    @Autowired
+    OwnerService ownerService;
 
     @GetMapping({"/owners","/owners/index","/owners/index.html"})
 
