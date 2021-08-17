3b2c9f1c843962dece7710781e6ad74106cbdc2e
jianheMark <jianhe.mark@pm.me>
2021-08-17 13:24:32 +0530

    create SpecialtiesService and implement SpecialityServiceMap class. closes #46

---
+++ springpetdata/src/main/java/jian/he/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package jian.he.services;
+
+import jian.he.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ springpetdata/src/main/java/jian/he/services/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package jian.he.services.map;
+
+import jian.he.model.Specialty;
+import jian.he.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+}
