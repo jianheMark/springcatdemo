24e3147861212bc7a3a355fddd8dcd7b46080c8e
jianheMark <jianhe.mark@pm.me>
2021-08-18 21:37:16 +0530

    create spring data jpa service for owners. closes #40

---
+++ springpetdata/src/main/java/jian/he/services/springdatajpa/OnwerJpaService.java
@@ -0,0 +1,57 @@
+package jian.he.services.springdatajpa;
+
+import jian.he.model.Owner;
+import jian.he.repositories.OwnerRepository;
+import jian.he.repositories.PetRepository;
+import jian.he.repositories.PetTypeRepository;
+import jian.he.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OnwerJpaService implements OwnerService {
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OnwerJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                           PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return new HashSet<>(ownerRepository.findAll());
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+}
