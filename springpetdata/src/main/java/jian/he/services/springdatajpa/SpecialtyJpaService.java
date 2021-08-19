8db563be8c1c8488d1e503a3baf53a2f95e31e2f
jianheMark <jianhe.mark@pm.me>
2021-08-19 10:41:30 +0530

    Create Spring Data JPA service for vet specialty. closes #44

---
+++ springpetdata/src/main/java/jian/he/services/springdatajpa/SpecialtyJpaService.java
@@ -0,0 +1,45 @@
+package jian.he.services.springdatajpa;
+
+import jian.he.model.Specialty;
+import jian.he.repositories.SpecialtyRepository;
+import jian.he.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtyJpaService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtyJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        return new HashSet<>(specialtyRepository.findAll());
+    }
+}
