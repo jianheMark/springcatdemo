c933603f1feb79019d82ebf34fa8d3a5502482fb
jianheMark <jianhe.mark@pm.me>
2021-08-19 11:41:09 +0530

    load visit on bootstrap. closes #50.

--- springpetweb/src/main/java/jian/he/bootstrap/DataLoader.java
+++ springpetweb/src/main/java/jian/he/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package jian.he.bootstrap;
 
 import jian.he.model.*;
-import jian.he.services.OwnerService;
-import jian.he.services.PetTypeService;
-import jian.he.services.SpecialtyService;
-import jian.he.services.VetService;
+import jian.he.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
 
@@ -85,8 +84,14 @@ public class DataLoader implements CommandLineRunner {
         fionasCat.setPetType(savedCatPetType);
         owner2.getPets().add(fionasCat);
 
-
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Cat Visit. Sneeze kitty");
+
+        visitService.save(catVisit);
         System.out.println("|||| Loading owners.");
 
         Vet vet1 = new Vet();
